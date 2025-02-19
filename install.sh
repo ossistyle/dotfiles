333f096068a69a5f7aa479eca8aee991473eccb2
Thomas Hoffmann <ossistyle@gmail.com>
2025-02-19 14:29:53 +0100

    📝 (dot_p10k.zsh.tmpl): update POWERLEVEL9K_INSTANT_PROMPT setting from verbose to quit for better performance during zsh initialization ♻️ (install.sh): remove echo statement to clean up script output and improve clarity

--- install.sh
+++ install.sh
@@ -19,6 +19,6 @@ fi
 
 # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
 script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
-echo "$script_dir"
+
 # exec: replace current process with chezmoi init
 exec "$chezmoi" init --apply "--source=$script_dir"
