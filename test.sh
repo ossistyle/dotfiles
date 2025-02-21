0df23da393bb249f77956c1ea6e1cfbbc4176675
Thomas Hoffmann <ossistyle@gmail.com>
2025-02-21 12:50:12 +0100

    🔧 (chezmoi.toml.tmpl): comment out conditional block for better readability and maintainability 🔧 (test.sh): replace docker run command with exec to improve process handling and resource management

--- test.sh
+++ test.sh
@@ -14,4 +14,4 @@ main
 # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
 script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
 
-docker run --rm -it --env DEBUG_DOTFILES=1 -v "$script_dir":/home/vscode/dotfiles --user vscode --workdir /home/vscode mcr.microsoft.com/devcontainers/base:ubuntu bash -c 'sh /home/vscode/dotfiles/install.sh && bash'
+exec docker run --rm -it --env DEBUG_DOTFILES=1 -v "$script_dir":/home/vscode/dotfiles --user vscode --workdir /home/vscode mcr.microsoft.com/devcontainers/base:ubuntu bash -c 'sh /home/vscode/dotfiles/install.sh && bash'
