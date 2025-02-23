5089afbfe144a7833e904db73fa970f396eaf405
Thomas Hoffmann <ossistyle@gmail.com>
2025-02-23 17:21:12 +0100

    ♻️ (run_install_test.sh): remove kcov uninstallation command to streamline the script and avoid unnecessary operations

--- scripts/run_install_test.sh
+++ scripts/run_install_test.sh
@@ -21,7 +21,6 @@ function merge_coverage_results() {
 }
 
 function uninstall_dependencies() {
-    command -v kcov && uninstall_kcov
     command -v bats && sudo apt-get remove -y bats parallel
 }
 
